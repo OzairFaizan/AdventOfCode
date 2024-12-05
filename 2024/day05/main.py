33ab1e9866c332c8f69f1cdc771a2b841aa5e3fc
ozairfzn <ahmad.faizan@epfl.ch>
2024-12-05 08:14:13 +0100

    Day 5 part 1 simplified with part 2's answer

--- 2024/day05/main.py
+++ 2024/day05/main.py
@@ -2,85 +2,42 @@ from collections import defaultdict
 import sys
 
 
-def part_one():
-    table = sys.stdin.readlines()
-    g = defaultdict(set)
+def toposort(g, v, visited, topo_order, order):
+    visited.add(v)
+    for n in g[v]:
+        if n in order and n not in visited:
+            toposort(g, n, visited, topo_order, order)
+    topo_order.append(v)
 
-    i = 0
-    while True:
-        if table[i] == "\n":
-            i += 1
-            break
-        a, b = list(map(int, table[i].strip("\n").split("|")))
-        g[b].add(a)
+
+table = sys.stdin.readlines()
+g = defaultdict(set)
+
+i = 0
+while True:
+    if table[i] == "\n":
         i += 1
+        break
+    a, b = list(map(int, table[i].strip("\n").split("|")))
+    g[b].add(a)
+    i += 1
 
-    ans = 0
-    while True:
-        if i >= len(table):
-            break
-        order = list(map(int, table[i].strip("\n").split(",")))
-        correct = True
-        for j in range(len(order)):
-            for k in range(j+1, len(order)):
-                if order[k] in g[order[j]]:
-                    correct = False
-                    break
-            if not correct:
-                break
-        if correct:
-            ans += order[len(order)//2]
-        i += 1
-    return ans
+part_one = 0
+part_two = 0
+while True:
+    if i >= len(table):
+        break
+    order = list(map(int, table[i].strip("\n").split(",")))
+    visited = set()
+    topo_order = []
+    for v in order:
+        if v not in visited:
+            toposort(g, v, visited, topo_order, order)
+    if topo_order == order:
+        part_one += topo_order[len(topo_order)//2]
+    else:
+        part_two += topo_order[len(topo_order)//2]
+    i += 1
 
-
-def part_two():
-    def toposort(g, v, visited, topo_order, order):
-        visited.add(v)
-        for n in g[v]:
-            if n in order and n not in visited:
-                toposort(g, n, visited, topo_order, order)
-        topo_order.append(v)
-
-    table = sys.stdin.readlines()
-    g = defaultdict(set)
-
-    i = 0
-    while True:
-        if table[i] == "\n":
-            i += 1
-            break
-        a, b = list(map(int, table[i].strip("\n").split("|")))
-        g[b].add(a)
-        i += 1
-
-    ans = 0
-    while True:
-        if i >= len(table):
-            break
-        order = list(map(int, table[i].strip("\n").split(",")))
-        correct = True
-        for j in range(len(order)):
-            for k in range(j+1, len(order)):
-                if order[k] in g[order[j]]:
-                    correct = False
-                    break
-            if not correct:
-                break
-        if correct:
-            i += 1
-            continue
-
-        visited = set()
-        topo_order = []
-        for v in order:
-            if v not in visited:
-                toposort(g, v, visited, topo_order, order)
-        ans += topo_order[len(topo_order)//2]
-        i += 1
-    return ans
-
-
-if __name__ == "__main__":
-    # print(part_one())
-    print(part_two())
+print(part_one)
+print(part_two)
